418e709a38f132bbc85926f73563297da2d382bd
vuplea <oan.raduk@gmail.com>
2013-09-26 15:17:07 -0700

    Update tiny_zip.js

--- tiny_zip.js
+++ tiny_zip.js
@@ -15,7 +15,11 @@ function tiny_zip()
 	var central_offset = 0;
 	this.add = function(nameStr, content)
 	{
-		var name = array_from_str(nameStr);
+		var utf8array_from_str = function(string)
+		{
+			return uint8array_from_binstr(unescape(encodeURIComponent(string)));
+		};
+		var name = utf8array_from_str(nameStr.replace(/[\/\:*?"<>\\|]/, "").slice(0, 255));
 		var nlen = name.length;
 		var clen = content.length;
 		var crc = crc32(content);
@@ -24,7 +28,7 @@ function tiny_zip()
 		crc, crc >> 8, crc >> 16, crc >> 24, clen, clen >> 8, clen >> 16, clen >> 24,
 		clen, clen >> 8, clen >> 16, clen >> 24, nlen, nlen >> 8, 0x00, 0x00]);
 		localH.set(name, 30);
-		//...content...
+		//
 		var centralH = new Uint8Array(46 + nlen);
 		var loff = local_offset;
 		centralH.set([0x50, 0x4b, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -62,21 +66,74 @@ function tiny_zip()
 		//
 		if (type == "blob")
 			return new Blob(outQueue, {type: "data:application/zip"});
-		else
+		else if (type == "array")
 		{
 			var output = new Uint8Array(local_offset + central_offset + 22);
-			for (var i = 0, k = 0; i < outQueue.length; i++)
-			{
+			for (var i = 0, k = 0; i < outQueue.length; k += outQueue[i].length, ++i)
 				output.set(outQueue[i], k);
-				k += outQueue[i].length;
-			}
 			return output;
 		}
-	};
-	
-	var array_from_str = function(string)
-	{
-		return uint8array_from_binstr(unescape(encodeURIComponent(string.replace(/[\/\:*?"<>\\|]/), "").slice(0, 255)));
+		else // type == "base64"
+		{//disfunctional
+			function out_it()
+			{
+				this.arr_i = 0;
+				this.offset = 0;
+				this.inc = function()
+				{
+					if (this.offset < outQueue[this.arr_i].length)
+						++this.offset;
+					else
+					{
+						++this.arr_i;
+						this.offset = 0;
+					}
+				};
+				this.deref = function()
+				{
+					return outQueue[this.arr_i][this.offset];
+				};
+			}
+			//
+			function sixbit_out_it()
+			{
+				it = new out_it();
+				this.offset = 2;
+				this.inc = function()
+				{
+					if (this.offset == 6)
+						this.offset = 0;
+					else
+					{
+						it.inc();
+						this.offset += 2;						
+					}
+				};
+				var prev_byte = 0;
+				this.deref = function()
+				{
+					return (prev <<< (8 - this.offset) | it.deref() >>> this.offset & 0xFFFFFF);
+				};
+			}
+			//
+			outQueue.push([0]);
+			var output = [];
+			var b64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+\\";
+			it = new sixbit_out_it();
+			for (it.it.arr_i < outQueue.length; it.inc())
+				output += b64Table.charAt(it.deref);
+			switch(it.offset)
+			{
+			case 2:
+				output += "==";
+				break;
+			case 4:
+				output += "=";
+				break;
+			case 2:
+				output = output.slice(0, -1);
+			}
+			return output;
 	};
 	
 	var crcTable = function()
@@ -95,7 +152,7 @@ function tiny_zip()
 	{
 		var crc = -1;
 		for (var i = 0; i < data.length; ++i)
-			crc = (crc >>> 8) ^ crcTable[(crc ^ data[i]) & 0xFF];
+			crc = (crc >>> 8) ^ crcTable[crc ^ data[i] & 0xFF];
 		return ~crc;
 	};
 }
